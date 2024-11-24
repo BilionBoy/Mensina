16abf3652e626e0206a9325857182e6c2946e05c
Clara Helena <106986725+clarasenx@users.noreply.github.com>
2024-11-23 23:15:50 -0400

    adicionar dados no modal ao finalizar quiz

--- src/app/pages/realizar-quizz/realizar-quizz.component.ts
+++ src/app/pages/realizar-quizz/realizar-quizz.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import Swal from 'sweetalert2';
+import Swal, { SweetAlertIcon, SweetAlertOptions } from 'sweetalert2';
 import { QuizService } from '../../../services/quiz.service';
 import { IQuizState } from '../../../interfaces/IQuizState';
 import { ToastrService } from 'ngx-toastr';
@@ -10,160 +10,169 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-realizar-quizz',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './realizar-quizz.component.html',
-  styleUrl: './realizar-quizz.component.css'
+  styleUrl: './realizar-quizz.component.css',
 })
 export class RealizarQuizzComponent implements OnInit {
-  quizId: number = 0
+  quizId: number = 0;
+  quizIsFinished: boolean = false;
+  quizState: IQuizState = {} as IQuizState;
 
-  quizState: IQuizState = {} as IQuizState
+  answeredQuestions: IQuestion[] = [];
+  notAnsweredQuestions: IQuestion[] = [];
+  questions: IQuestion[] = [];
 
-  answeredQuestions: IQuestion[] = []
-  notAnsweredQuestions: IQuestion[] = []
-  questions: IQuestion[] = []
-
-  answeredQuestionsId: number[] = []
-
-  questionId: number = 0
+  answeredQuestionsId: number[] = [];
 
   colors: Record<number, string> = {
     1: '#FCD5D6',
     2: '#E1ECE4',
     3: '#D3D4FE',
     4: '#FAEFB6',
-  }
+  };
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private quizService: QuizService,
     private toast: ToastrService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
-    this.quizId = parseInt(this.route.snapshot.paramMap.get('id') || '0')
+    this.quizId = parseInt(this.route.snapshot.paramMap.get('id') || '0');
 
     if (this.quizId === 0 || isNaN(this.quizId)) {
-      this.router.navigate(['/'])
-      return
+      this.router.navigate(['/']);
+      return;
     }
-    this.getQuizState(this.quizId)
-    this.getQuestions(this.quizId)
+    this.getQuizState();
+    this.getQuestions();
   }
 
-  getQuizState(quizId: number) {
-    this.quizService.getQuizById(quizId)
-      .subscribe({
-        next: (res) => {          
-          this.answeredQuestionsId = Object.keys(res.questions).map(q => parseInt(q))
-          this.quizState = res
-          this.updateQuestionsList()
-        },
-        error: err => {
-          this.toast.error("Erro ao carregar dados do quiz")
-          this.router.navigate(['/'])
-        }
-      })
+  getQuizState() {
+    this.quizService.getQuizById(this.quizId).subscribe({
+      next: (res) => {
+        this.answeredQuestionsId = Object.keys(res.questions).map((q) =>
+          parseInt(q)
+        );
+        this.quizState = res;
+        this.updateQuestionsList();
+      },
+      error: (err) => {
+        this.toast.error('Erro ao carregar dados do quiz');
+        this.router.navigate(['/']);
+      },
+    });
   }
 
-  getQuestions(quizId: number) {
-    this.quizService.getQuestions(quizId)
-      .subscribe({
-        next: (res) => {
-          this.questions = res
-          this.updateQuestionsList()
-        },
-        error: err => {
-          this.toast.error("Erro ao carregar dados do quiz")
-          this.router.navigate(['/'])
-        }
-      })
+  getQuestions() {
+    this.quizService.getQuestions(this.quizId).subscribe({
+      next: (res) => {
+        this.questions = res;
+        this.updateQuestionsList();
+      },
+      error: (err) => {
+        this.toast.error('Erro ao carregar dados do quiz');
+        this.router.navigate(['/']);
+      },
+    });
   }
 
   updateQuestionsList() {
-    this.answeredQuestions = []
-    this.notAnsweredQuestions = []
-    this.questions.forEach(question => {
+    this.answeredQuestions = [];
+    this.notAnsweredQuestions = [];
+    this.questions.forEach((question) => {
       if (this.answeredQuestionsId.includes(question.id)) {
-        this.answeredQuestions.push(question)
+        this.answeredQuestions.push(question);
       } else {
-        this.notAnsweredQuestions.push(question)
+        this.notAnsweredQuestions.push(question);
       }
-    })
-  }
-
-  isCorrect() {
-    Swal.fire({
-      title: "Resposta correta!",
-      icon: 'success',
-      width: 600,
-      padding: "3em",
-      color: "#000",
-      background: "#fff",
-      confirmButtonText: this.notAnsweredQuestions.length <= 1 ? 'Ir para home' : 'Próximo',
-      text: this.notAnsweredQuestions.length <= 1 ? 'Quiz finalizado' : '',
-      backdrop: `
-          rgba(0,123,0,0.2)
-          url(/gifs/nyan-cat-poptart-cat.gif)
-          left top
-          no-repeat
-        `
-    }).then(() => {
-      if(this.notAnsweredQuestions.length <= 1) {
-        this.router.navigate(['/'])
-        return
-      }
-      this.questionId++
-      this.getQuizState(this.quizId)
     });
   }
 
-  isIncorrect() {
-    Swal.fire({
-      title: "Resposta incorreta!",
-      icon: 'error',
-      width: 600,
-      padding: "3em",
-      color: "#000",
-      background: "#fff",
-      confirmButtonText: this.notAnsweredQuestions.length <= 1 ? 'Ir para home' : 'Próximo',
-      text: this.notAnsweredQuestions.length <= 1 ? 'Quiz finalizado' : '',
-      backdrop: `
-          rgba(0,0,123,0.3)
-          url(/gifs/stitch-sad-sad-stitch-unscreen.gif)
-          left top
-          no-repeat
-        `,
+  getAlertOptions(status: 'success' | 'error'): SweetAlertOptions {
+    const title =
+      status === 'success' ? 'Resposta correta!' : 'Resposta incorreta!';
+    const confirmButtonText = this.quizIsFinished ? 'Ir para home' : 'Próximo';
 
-    }).then(() => {
-      if(this.notAnsweredQuestions.length <= 1) {
-        this.router.navigate(['/'])
-        return
-      }
-      this.questionId++
-      this.getQuizState(this.quizId)
-    });
+    const html = this.quizIsFinished
+      ? `
+    <div>
+    <p>Média de acertos: ${(
+      (this.quizState.correct / this.quizState.total) *
+      100
+    ).toFixed(0)} %</p>
+    <p>Total de acertos: ${this.quizState.correct}</p>
+    <p>Pontuação: ${this.quizState.score} XP</p>
+    </div>
+    `
+      : undefined;
+
+    const backdrop =
+      (status === 'success'
+        ? `
+      rgba(0,123,0,0.2)
+      url(/gifs/nyan-cat-poptart-cat.gif)`
+        : `
+        rgba(0,0,123,0.3)
+        url(/gifs/stitch-sad-sad-stitch-unscreen.gif)`) +
+      `
+    left top
+    no-repeat
+  `;
+    return {
+      title,
+      icon: status,
+      width: 600,
+      padding: '3em',
+      color: '#000',
+      background: '#fff',
+      confirmButtonText,
+      html,
+      backdrop,
+    };
+  }
+
+  alertCallback() {
+    if (this.quizIsFinished) {
+      this.router.navigate(['/']);
+      return;
+    }
+    this.getQuizState();
   }
 
   checkAnswer(answerId: number) {
-    const questionId = this.notAnsweredQuestions[0].id
-    this.quizService.checkAnswer(questionId, answerId)
-      .subscribe({
-        next: (res) => {
-          if (res.is_correct) {
-            this.isCorrect()
-          } else {
-            this.isIncorrect()
+    const questionId = this.notAnsweredQuestions[0].id;
+    this.quizService.checkAnswer(questionId, answerId).subscribe({
+      next: async (res) => {
+        if (res.is_correct) {
+          if (this.notAnsweredQuestions.length <= 1) {
+            this.quizIsFinished = true;
+            this.quizState = await this.finishQuiz();
+            console.log('quiz finalizado: ', this.quizState);
           }
-        },
-        error: err => {
-          this.toast.error('Erro ao verificar questão')
+          Swal.fire(this.getAlertOptions('success')).then(() =>
+            this.alertCallback()
+          );
+        } else {
+          Swal.fire(this.getAlertOptions('error')).then(() =>
+            this.alertCallback()
+          );
         }
-      })
+      },
+      error: (err) => {
+        this.toast.error('Erro ao verificar questão');
+      },
+    });
+  }
+
+  finishQuiz() {
+    return new Promise<IQuizState>((resolve) => {
+      this.quizService.finishQuiz(this.quizId).subscribe({
+        next: (res) => resolve(res),
+      });
+    });
   }
 
   close() {
@@ -178,8 +187,8 @@ export class RealizarQuizzComponent implements OnInit {
       cancelButtonText: 'Cancelar',
     }).then((result) => {
       if (result.isConfirmed) {
-        this.router.navigate(['/'])
+        this.router.navigate(['/']);
       }
-    })
+    });
   }
 }
--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -5,31 +5,48 @@ import { IQuizState } from '../interfaces/IQuizState';
 import { IQuestion } from '../interfaces/IQuestion';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuizService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getQuizzes(params?: {
+    tagId?: number;
+    inProgres?: 'inProgres' | 'notInProgres';
+  }): Observable<any> {
+    let url = !params?.tagId
+      ? 'http://localhost:5000/quiz/'
+      : `http://localhost:5000/quiz/?tag_id=${params.tagId}`;
 
-  getQuizzes(params?: { tagId?: number, inProgres?: 'inProgres' | 'notInProgres' }): Observable<any> {
-    let url = !params?.tagId ?
-      'http://localhost:5000/quiz/' :
-      `http://localhost:5000/quiz/?tag_id=${params.tagId}`
-
-    url += !params?.tagId ? '?' : '&'
-    url += `in_progress=${params?.inProgres === 'inProgres'}`
-    return this.http.get(url)
+    url += !params?.tagId ? '?' : '&';
+    url += `in_progress=${params?.inProgres === 'inProgres'}`;
+    return this.http.get(url);
   }
 
   getQuizById(quizId: number): Observable<IQuizState> {
-    return this.http.get<IQuizState>(`http://localhost:5000/quiz/start/${quizId}`)
+    return this.http.get<IQuizState>(
+      `http://localhost:5000/quiz/start/${quizId}`
+    );
   }
-  
+
   getQuestions(quizId: number): Observable<IQuestion[]> {
-    return this.http.get<IQuestion[]>(`http://localhost:5000/quiz/questions/${quizId}`)
+    return this.http.get<IQuestion[]>(
+      `http://localhost:5000/quiz/questions/${quizId}`
+    );
   }
-  
-  checkAnswer(questionId: number, answerId: number): Observable<{is_correct: boolean}> {
-    return this.http.get<{is_correct: boolean}>(`http://localhost:5000/quiz/answer_check?answer_id=${answerId}&question_id=${questionId}`)
+
+  checkAnswer(
+    questionId: number,
+    answerId: number
+  ): Observable<{ is_correct: boolean }> {
+    return this.http.get<{ is_correct: boolean }>(
+      `http://localhost:5000/quiz/answer_check?answer_id=${answerId}&question_id=${questionId}`
+    );
+  }
+
+  finishQuiz(quizId: number): Observable<IQuizState> {
+    return this.http.delete<IQuizState>(
+      `http://localhost:5000/quiz/finish/${quizId}`
+    );
   }
 }
