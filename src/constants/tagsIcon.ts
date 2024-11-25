35c367cd80bcc23216d1cdb2d9a3c361f239d8a8
Camila Borges Venâncio <borgescamila09@gmail.com>
2024-11-25 08:43:19 -0400

    arrumando icones e cores das tags

--- src/constants/tagsIcon.ts
+++ src/constants/tagsIcon.ts
@@ -2,8 +2,12 @@ export interface ITagIcon {color: string, icon: string}
 
 export const tagsIcons: Record<number, ITagIcon> = {
   0: {color: '#FCD5D6', icon: 'window'},
-  1: {color: '#FCD5D6', icon: 'database'},
-  2: {color: '#E1ECE4', icon: 'computer'},
+  10: {color: '#FAEFB6', icon: 'database'},
+  11: {color: '#E1ECE4', icon: 'security'},
   6: {color: '#D3D4FE', icon: 'psychology_alt'},
-  4: {color: '#FAEFB6', icon: 'stylus_note'}
+  9: {color: '#FCD5D6', icon: 'language'}
 }
+
+
+
+
