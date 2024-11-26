001fff68abe7424c5ab5eb87080912aa1cb9e1ff
LEONARDO VINICIUS SILVA BATISTA <leovinicius3737@gmail.com>
2024-11-26 08:44:03 -0400

    alterar tagIcons para novos ids

--- src/constants/tagsIcon.ts
+++ src/constants/tagsIcon.ts
@@ -2,12 +2,11 @@ export interface ITagIcon {color: string, icon: string}
 
 export const tagsIcons: Record<number, ITagIcon> = {
   0: {color: '#FCD5D6', icon: 'window'},
-  10: {color: '#FAEFB6', icon: 'database'},
-  11: {color: '#E1ECE4', icon: 'security'},
-  6: {color: '#D3D4FE', icon: 'psychology_alt'},
-  9: {color: '#FCD5D6', icon: 'language'}
+  1: {color: '#FAEFB6', icon: 'database'},
+  2: {color: '#E1ECE4', icon: 'security'},
+  3: {color: '#D3D4FE', icon: 'psychology_alt'},
+  4: {color: '#FCD5D6', icon: 'language'},
+  5: {color: '#FFCBDF', icon: 'crisis_alert'}
 }
-
-
 
 
