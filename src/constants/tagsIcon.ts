31fb2cee1773e1987aa7c9818ca989056bc3b7df
Camila Borges Venâncio <borgescamila09@gmail.com>
2024-11-26 09:49:07 -0400

    trocando icons

--- src/constants/tagsIcon.ts
+++ src/constants/tagsIcon.ts
@@ -4,9 +4,9 @@ export const tagsIcons: Record<number, ITagIcon> = {
   0: {color: '#FCD5D6', icon: 'window'},
   1: {color: '#FAEFB6', icon: 'database'},
   2: {color: '#E1ECE4', icon: 'security'},
-  3: {color: '#D3D4FE', icon: 'psychology_alt'},
-  4: {color: '#FCD5D6', icon: 'language'},
-  5: {color: '#FFCBDF', icon: 'crisis_alert'}
+  3: {color: '#D3D4FE', icon: 'code'},
+  4: {color: '#FCD5D6', icon: 'terminal'},
+  5: {color: '#D0DDED', icon: 'crisis_alert'}
 }
 
 
