0e133758f349c5a6f3904826b2513c6067e3728e
LEONARDO VINICIUS SILVA BATISTA <leovinicius3737@gmail.com>
2024-11-26 12:16:00 -0400

    adicionar algoritmo em tagIcons

--- src/constants/tagsIcon.ts
+++ src/constants/tagsIcon.ts
@@ -6,7 +6,8 @@ export const tagsIcons: Record<number, ITagIcon> = {
   2: {color: '#E1ECE4', icon: 'security'},
   3: {color: '#D3D4FE', icon: 'code'},
   4: {color: '#FCD5D6', icon: 'terminal'},
-  5: {color: '#D0DDED', icon: 'crisis_alert'}
+  5: {color: '#D0DDED', icon: 'crisis_alert'},
+  6: {color: '#FFE2C5', icon: 'psychology'},
 }
 
 
